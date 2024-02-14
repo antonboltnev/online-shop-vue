5f9e55673e4423131385b293d3f16e5e21f7ac66
João Lucas Crescencio Granjeiro Barbosa <joaolucascgbarbosa878@gmail.com>
2024-02-14 10:10:43 -0300

    first commit

--- src/main.js
+++ src/main.js
@@ -1,25 +1,24 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import './assets/styles.css'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import "./assets/styles.css";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+const app = createApp(App);
 
-// Vuetify
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
 
 const vuetify = createVuetify({
-    components,
-    directives,
-})
+  components,
+  directives,
+});
 
-app.use(createPinia())
-app.use(router)
-app.use(vuetify)
+app.use(createPinia());
+app.use(router);
+app.use(vuetify);
 
-app.mount('#app')
+app.mount("#app");
--- src/views/Catalog.vue
+++ src/views/Catalog.vue
@@ -1,55 +1,47 @@
 <template>
   <div class="products-list">
-    <v-text-field clearable label="Label" prepend-icon="$vuetify"></v-text-field>
     <v-row no-gutters>
       <v-col
-          v-for="product in store.products"
-          :key="product.id"
-          cols="12"
-          sm="4"
-          @click="goToProductPage(product.id)"
+        v-for="product in store.products"
+        :key="product.id"
+        cols="12"
+        sm="4"
+        @click="goToProductPage(product.id)"
       >
-        <product-item
-            :product-data="product"
-            @item-clicked="goToProductPage"
-        />
+        <product-item :product-data="product" @item-clicked="goToProductPage" />
       </v-col>
     </v-row>
-
-    </div>
+  </div>
 </template>
 
 <script>
-  import { defineComponent } from "vue";
-  import ProductItem from "@/components/ProductItem.vue";
-  export default defineComponent({
-    name: 'CatalogView',
-    components: {
-      ProductItem
-    }
-  })
+import { defineComponent } from "vue";
+import ProductItem from "@/components/ProductItem.vue";
+export default defineComponent({
+  name: "CatalogView",
+  components: {
+    ProductItem,
+  },
+});
 </script>
 
 <script setup>
-  import { onMounted, ref } from "vue";
-  import { productsStore } from "@/stores/products";
-  import { useRouter } from "vue-router";
+import { onMounted, ref } from "vue";
+import { productsStore } from "@/stores/products";
+import { useRouter } from "vue-router";
 
-  const store = productsStore()
-  const router = useRouter()
+const store = productsStore();
+const router = useRouter();
 
-  const search = ref('')
+const search = ref("");
 
-  const goToProductPage = (id) => {
-    router.push({ name: 'ProductView', params: { id } })
-  }
+const goToProductPage = (id) => {
+  router.push({ name: "ProductView", params: { id } });
+};
 
-
-  onMounted(async () => {
-    await store.fetchProductsFromDB()
-  })
+onMounted(async () => {
+  await store.fetchProductsFromDB();
+});
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
